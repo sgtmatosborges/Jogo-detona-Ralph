ce087dbf87571c3c80160a53c685994ca388be11
matosborges39 <73673323+sgtmatosborges@users.noreply.github.com>
2024-11-14 17:24:47 -0300

    Update engine.js
    JavaScript para funcionamento do jogo.

---
+++ src/scripts/engine.js
@@ -0,0 +1,74 @@
+const state = {
+    view: {
+      squares: document.querySelectorAll(".square"),
+      enemy: document.querySelector(".enemy"),
+      timeLeft: document.querySelector("#time-left"),
+      score: document.querySelector("#score"),
+    },
+    values:{
+       gameVelocity: 1000,
+       hitPosition: 0,
+       result: 0,
+       currentTime: 60,
+    },
+    actions:{
+       timeId: setInterval(randomSquare, 1000), 
+       countDownTimeId: setInterval(countDown, 1000),
+    }
+
+};
+
+function countDown(){
+   state.values.currentTime--;
+   state.view.timeLeft.textContent = state.values.currentTime;
+   
+   
+   if(state.values.currentTime < 0){
+      clearInterval(state.actions.countDownTimeId);
+      clearInterval(state.actions.timeId);
+      alert("Game Over! O seu resultado foi: " + state.values.result);
+   }
+}
+
+
+function randomSquare(){
+    state.view.squares.forEach((square)=>{
+       square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+ function playSound(){
+    let audio = new Audio("./src/audios/hit.m4a");
+    audio.volume = 0.2;
+    audio.play();
+ }
+
+// function moveEnemy(){
+
+//    state.values.timeId = setInterval(randomSquare, state.values.gameVelocity); 
+// }
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown",() => {
+            if(square.id === state.values.hitPosition){
+               state.values.result++
+               state.view.score.textContent = state.values.result;
+               state.values.hitPosition = null;
+               playSound();
+            }
+        });
+    });
+}
+
+function initialize() {
+    // moveEnemy();
+
+    addListenerHitBox();
+}
+
+initialize();
